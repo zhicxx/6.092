b0238b55123181621fb70ca77e5beda59bbb95cc
陈智 <chenzhi@MacBook-Pro-7.local>
2025-11-20 11:31:53 +0000

    Track all java files properly

---
+++ 4/Library.java
@@ -0,0 +1,114 @@
+public class Library {
+    String address;
+    Book[] books;
+    int bookCount;
+
+    public Library(String address){
+        this.address = address;
+        this.books = new Book[100];
+        this.bookCount = 0;
+    }
+    
+    public void addBook(Book book){
+        this.books[this.bookCount] = book;
+        this.bookCount++;
+    }
+
+    public void printAddress(){
+        System.out.println(this.address);
+    }
+
+    public static void printOpeningHours(){
+        System.out.println("Libraries are open daily from 9am to 5pm.");
+    }
+
+    public void borrowBook(String title){
+        for (int i =0; i < this.bookCount; i++){
+            Book book = this.books[i];
+
+            if (book.getTitle().equals(title)){
+                if(book.isBorrowed()){
+                    System.out.println("Sorry, this book is already borrowed.");
+                }else{
+                    book.borrowed();
+                    System.out.println("You successfully borrowed " + title);
+                }
+                return;
+            }
+        }
+        System.out.println("Sorry, this book is not in our catalog.");
+    }
+
+    public void returnBook(String title){
+        for (int i = 0; i < this.bookCount; i++){
+            Book book = this.books[i];
+
+            if(book.getTitle().equals(title)){
+                book.returned();
+                System.out.println("You successfully returned" + title);
+                return;
+            }
+        }
+        System.out.println("Sorry, this book is not in our catalog.");
+    }
+
+    public void printAvailableBooks(){
+        if (this.bookCount == 0){
+            System.out.println("No book in catalog");
+            return;
+        }
+
+        for (int i = 0; i < this.bookCount; i++){
+            Book book = this.books[i];
+            
+            if (!book.isBorrowed()){
+                System.out.println(book.getTitle());
+            }
+        }
+    }
+    public static void main(String[] args) {
+        // Create two libraries
+        Library firstLibrary = new Library("10 Main St.");
+        Library secondLibrary = new Library("228 Liberty St.");
+
+        // Add four books to the first library
+        firstLibrary.addBook(new Book("The Da Vinci Code"));
+        firstLibrary.addBook(new Book("Le Petit Prince"));
+        firstLibrary.addBook(new Book("A Tale of Two Cities"));
+        firstLibrary.addBook(new Book("The Lord of the Rings"));
+
+        // Print opening hours and the addresses
+        System.out.println("Library hours:");
+        printOpeningHours();
+        System.out.println();
+
+        System.out.println("Library addresses:");
+        firstLibrary.printAddress();
+        secondLibrary.printAddress();
+        System.out.println();
+
+        // Try to borrow The Lords of the Rings from both libraries
+        System.out.println("Borrowing The Lord of the Rings:");
+        firstLibrary.borrowBook("The Lord of the Rings");
+        firstLibrary.borrowBook("The Lord of the Rings");
+        secondLibrary.borrowBook("The Lord of the Rings");
+        System.out.println();
+
+        // Print the titles of all available books from both libraries
+        System.out.println("Books available in the first library:");
+        firstLibrary.printAvailableBooks();
+        System.out.println();
+        System.out.println("Books available in the second library:");
+        secondLibrary.printAvailableBooks();
+        System.out.println();
+
+        // Return The Lords of the Rings to the first library
+        System.out.println("Returning The Lord of the Rings:");
+        firstLibrary.returnBook("The Lord of the Rings");
+        System.out.println();
+
+        // Print the titles of available from the first library
+        System.out.println("Books available in the first library:");
+        firstLibrary.printAvailableBooks();
+    }
+} 
